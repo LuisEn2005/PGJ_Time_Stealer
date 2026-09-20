d6fc9efc9331c13eb2dbb478ed5da6c306cd8aa9
LuisEn2005 <luisenramos0805@gmail.com>
2026-09-20 10:31:44 -0500

    First add of textures (needs fixing)

--- main.cpp
+++ main.cpp
@@ -20,6 +20,13 @@ int main(void) {
   bool gameOver = false;
   float gameSpeed = 300.0f;
 
+  Image clavaSheet = LoadImage("./Entities/obsSprites/clava_pairs.png");
+  
+  Texture2D clava_tex = LoadTextureFromImage(clavaSheet);
+
+  UnloadImage(clavaSheet);
+  int simpleVar, highVar;
+
   Timer spawnTimer;
   TimerStart(&spawnTimer, 1.5f);
 
@@ -33,7 +40,13 @@ int main(void) {
 
       if(TimerDone(&spawnTimer)){
         ObstacleType type = static_cast<ObstacleType>(rand() % 3);
-        obstaculos.push_back(Obstacle(900.0f, floorY, type));
+        if(type == ObstacleType::DOBLE_BAJO){
+          highVar = rand() % 16;
+          obstaculos.push_back(Obstacle(900.0f, floorY, type, clava_tex, highVar));
+        } else{
+          simpleVar = rand() % 4; 
+          obstaculos.push_back(Obstacle(900.0f, floorY, type, clava_tex, simpleVar));
+        }
 
         float nextSpawnTime = 1.2f + ((float)rand() / RAND_MAX) * 0.8f;
         TimerStart(&spawnTimer, nextSpawnTime);
